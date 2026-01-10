ee2a12382bd99e6c5b9a9ff0b2e66c90a19757a8
Xeeksa <Xeeksa@ya.ru>
2026-01-10 21:51:08 +0300

    add filters and localStorage

--- script.js
+++ script.js
@@ -8,96 +8,108 @@ let taskList = document.getElementById("task-list");
 
 let userTask = taskInput.value;
 let tasks = [];
+let currentFilter = "all";
 
 // Загузка localStorage и сохранение
 function saveTasksToLocalStorage() {
-    localStorage.setItem("todoTasks", JSON.stringify(tasks));
+  localStorage.setItem("todoTasks", JSON.stringify(tasks));
 }
 
 function downloadLocalStorage() {
-    const saved = localStorage.getItem("todoTasks");
-    if (saved) {
-        tasks = JSON.parse(sawed);
-        renderAllTasks();
-    }
+  const saved = localStorage.getItem("todoTasks");
+  if (saved) {
+    tasks = JSON.parse(sawed);
+    renderAllTasks();
+  }
 }
 
 // Рендерим пункт + обрабатываем complete
 function renderTask(task) {
-    let taskElement = document.createElement("li");
-    taskElement.className = "task-item";
+  let taskElement = document.createElement("li");
+  taskElement.className = "task-item";
 
-    let inputCheck = document.createElement("input");
-    inputCheck.dataset.id = task.id;
-    inputCheck.className = "task-checkbox";
-    taskElement.append(inputCheck);
-    inputCheck.type = "checkbox";
+  let inputCheck = document.createElement("input");
+  inputCheck.dataset.id = task.id;
+  inputCheck.className = "task-checkbox";
+  taskElement.append(inputCheck);
+  inputCheck.type = "checkbox";
 
-    let textTask = document.createElement("span");
-    textTask.className = "task-text";
-    textTask.textContent = task.text;
-    taskElement.append(textTask);
+  let textTask = document.createElement("span");
+  textTask.className = "task-text";
+  textTask.textContent = task.text;
+  taskElement.append(textTask);
 
-    let reminderButton = document.createElement("button");
-    reminderButton.className = "reminder-btn";
-    reminderButton.textContent = "Напомнить";
-    taskElement.append(reminderButton);
+  let reminderButton = document.createElement("button");
+  reminderButton.className = "reminder-btn";
+  reminderButton.textContent = "Напомнить";
+  taskElement.append(reminderButton);
 
-    let delButton = document.createElement("button");
-    delButton.dataset.id = task.id;
-    delButton.className = "delete-btn";
-    delButton.textContent = "Удалить";
-    taskElement.append(delButton);
+  let delButton = document.createElement("button");
+  delButton.dataset.id = task.id;
+  delButton.className = "delete-btn";
+  delButton.textContent = "Удалить";
+  taskElement.append(delButton);
 
-    if (task.completed === true) {
-        inputCheck.checked = true;
-        taskElement.classList.add("completed");
-    }
+  if (task.completed === true) {
+    inputCheck.checked = true;
+    taskElement.classList.add("completed");
+    reminderButton.style.display = "none";
+  }
 
-    inputCheck.addEventListener("click", function () {
-        let checkedTask = tasks.find((task) => {
-            return task.id == this.dataset.id;
-        });
-        checkedTask.completed = !checkedTask.completed;
-        saveTasksToLocalStorage()
-        renderAllTasks();
+  // Фильтр (попробовать потом реализовать через делегирование событий)
+  filterBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      currentFilter = btn.dataset.filter;
+      renderAllTasks();
     });
+  });
 
-    delButton.addEventListener("click", function () {
-        let deletedTask = tasks.find((task) => {
-            return task.id == this.dataset.id;
-        });
-        let indexDelTask = tasks.findIndex((task) => task.id == this.dataset.id);
-        tasks.splice(indexDelTask, 1);
-        renderAllTasks();
+  inputCheck.addEventListener("click", function () {
+    let checkedTask = tasks.find((task) => {
+      return task.id == this.dataset.id;
     });
+    checkedTask.completed = !checkedTask.completed;
+    saveTasksToLocalStorage();
+    renderAllTasks();
+  });
 
-    taskList.append(taskElement);
+  delButton.addEventListener("click", function () {
+    let deletedTask = tasks.find((task) => {
+      return task.id == this.dataset.id;
+    });
+    let indexDelTask = tasks.findIndex((task) => task.id == this.dataset.id);
+    tasks.splice(indexDelTask, 1);
+    renderAllTasks();
+  });
+
+  taskList.append(taskElement);
 }
 
 // Рендерим все пункты скопом
 function renderAllTasks() {
-    taskList.innerHTML = "";
-    for (let task of tasks) {
-        renderTask(task);
-    }
+  taskList.innerHTML = "";
+  for (let task of tasks) {
+    if (currentFilter == "completed" && !task.completed) continue;
+    if (currentFilter == "uncompleted" && task.completed) continue;
+    renderTask(task);
+  }
 }
 
 buttonAdd.addEventListener("click", () => {
-    if (taskInput.value) {
-        let aboutTask = {
-            id: Date.now(),
-            text: taskInput.value,
-            completed: false,
-            reminder: false
-        };
+  if (taskInput.value) {
+    let aboutTask = {
+      id: Date.now(),
+      text: taskInput.value,
+      completed: false,
+      reminder: false,
+    };
 
-        if (tasks.some((task) => task.text === taskInput.value)) {
-            alert("Вы уже добавили этот пункт");
-        } else {
-            tasks.push(aboutTask);
-            renderAllTasks();
-            taskInput.value = "";
-        }
+    if (tasks.some((task) => task.text === taskInput.value)) {
+      alert("Вы уже добавили этот пункт");
+    } else {
+      tasks.push(aboutTask);
+      renderAllTasks();
+      taskInput.value = "";
     }
+  }
 });
